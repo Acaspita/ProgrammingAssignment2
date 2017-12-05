ada2e86f357cd8fc2dfa616c12132f1820f31926
NEORIS\leandro.altamirano <leandro.altamirano@neoris.com>
2017-12-05 15:45:46 -0300

    Se agregó un carpeta "subrepo", y se hicieron las modificaciones solicitadas en el archivo correspondiente.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates a special "matrix" object that can cache its
+## inverse
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(closureMatrix = matrix()) {
+    inversedMatrix <- NULL
+    set <- function(newMatrix) {
+        closureMatrix <<- newMatrix
+        inversedMatrix <<- NULL
+    }
+    get <- function() closureMatrix
+    setInverse <- function(inverse) {
+        inversedMatrix <- inverse
+    }
+    getInverse <- function() inversedMatrix
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned
+## by makeCacheMatrix. If the inverse has already been calculated and
+## it has not been modified, then the cachesolve should retrieve the
+## inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getInverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(, ...)
+    x$setInverse(m)
+    m
 }
